f1a119353b94c904ce56f41338eb313fc13e4fdc
Egor Pishchalnikov <e.pichtchalnikov@gmail.com>
2020-03-10 01:04:40 +0300

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
@@ -8,8 +8,12 @@ import { rootReducer } from './store/reducers';
 import createSagaMiddleware from 'redux-saga';
 import { watchFormSubmit } from './sagas';
 
+// use composeEnhancers to use redux-saga enhancer and redux devtools together
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(watchFormSubmit);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
