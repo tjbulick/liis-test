19b1e5537fe4e435c180008cf2c7eb8620bcac77
Egor Pishchalnikov <e.pichtchalnikov@gmail.com>
2020-03-16 23:29:43 +0300

    Add contacts

--- src/YouAreRegistred.js
+++ src/YouAreRegistred.js
@@ -6,6 +6,12 @@ const YouAreRegistred = (props) => {
         <div className="you-are-registred">
             <h1>You are successfully registred!</h1>
             <h3>Your unique ID in Firebase is {props.id}</h3>
+            <h3>Contact me, to check out database content: </h3>
+            Telegram: <a href="https://t.me/tjbulick" target="_blank">tjbulick</a>
+            <br/>
+            ВКонтакте: <a href="https://vk.com/tjbulick" target="_blank">tjbulick</a>
+            <br/>
+            E-mail: <a href="mailto:e.pichtchalnikov@gmail.com?subject=Доступ к Firebase">e.pichtchalnikov@gmail.com</a>
         </div>
     )
 }
